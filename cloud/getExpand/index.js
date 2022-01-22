63fd5f0f4789f42e3763343bb84102535677d2cd
1281203681@qq.com <1281203681@qq.com>
2022-01-22 22:14:43 +0800

    first

--- cloud/getExpand/index.js
+++
@@ -1,9 +0,0 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-cloud.init()
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-  return await cloud.database().collection("expand").where({count:event.count}).limit(1000).get()
-}
